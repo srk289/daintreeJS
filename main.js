96bd8a19559311c408b31dcafebbb4ebb3a5587f
Srikanth Kondaparthy <srk289@gmail.com>
2016-07-07 06:28:44 +1000

    Adds basics parts 1 and 2

---
+++ main.js
@@ -0,0 +1,18 @@
+// 1.1 Comment 
+document.write('hello world');
+document.write('<h1>Hello world</h1>');
+
+/*  // This is also comment
+	document.write('<h1>Hello world</h1>');
+*/
+
+// 1.2 Whitespaces and linebreaks and ; are optional
+// var1 = 10
+// var2 = 20
+// alert(var1+var2)
+
+// 1.3 simple function
+function namaste(name)
+{
+	alert('Namste Mr '+ name);
+}
